4c7542c5c380af783d54515d4f3f1a09bb8e2ddc
amsuy <161232769+amsuy@users.noreply.github.com>
2025-05-28 01:56:58 -0600

    Update CitaProgramadaServicio.java

--- src/main/java/com/registro/citas/servicio/CitaProgramadaServicio.java
+++ src/main/java/com/registro/citas/servicio/CitaProgramadaServicio.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
